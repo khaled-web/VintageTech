87c0f98c0ffd2e4b06497328a044b868eab704b0
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-09-14 14:03:40 +0200

    hero page - done

--- FrontEnd/src/components/Hero.js
+++ FrontEnd/src/components/Hero.js
@@ -1,5 +1,14 @@
 import React from "react";
+import '../assets/Css/hero.css'
 
-export default function Hero() {
-  return <h1>hello from hero</h1>;
+export default function Hero({children}) {
+  return(
+    <div className="hero">
+      <div className="banner">
+        <h1>think, code, deploy</h1>
+        <p>embrace your choices - we do</p>
+        {children}
+      </div>
+    </div>
+  )
 }
---
+++ FrontEnd/src/components/index.js
@@ -0,0 +1,27 @@
+import Alert from './Alert'
+import Header from './Header'
+import Hero from './Hero'
+import Loading from './Loading'
+import LoginLink from './LoginLink'
+import PrivateRoute from './PrivateRoute'
+import cartItem from './Cart/CartItem'
+import CartLink from './Cart/CartLink'
+import EmptyCart from './Cart/EmptyCart'
+import FeaturedProduct from './Products/FeaturedProducts'
+import Product from './Products/Product'
+import ProductList from './Products/ProductList'
+
+export {
+ Alert,
+ Header,
+ Hero,
+ Loading,
+ LoginLink,
+ PrivateRoute,
+ cartItem,
+ CartLink,
+ EmptyCart,
+ FeaturedProduct,
+ Product,
+ ProductList
+}
