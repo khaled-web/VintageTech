d4f87bd8b67a25f571a0f1a94de666a4fe7929ce
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2023-09-14 12:30:15 +0200

    Error,about-pages .... done

--- FrontEnd/src/pages/About.js
+++ FrontEnd/src/pages/About.js
@@ -1,5 +1,13 @@
 import React from "react";
+import '../assets/Css/about.css'
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return(
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        If you're looking for a reliable website to help you buy an electronic machine, look no further. We offer the best customer service and the most competitive prices in town. Our knowledgeable staff can answer any questions you may have about our products and services so that your purchase is stress-free and enjoyable. With our wide selection of machines from top brands, we will surely have something that will meet all your needs. Plus, we provide free shipping on orders over $100! So don't hesitate - come visit us today for all of your electronic machine needs!
+      </p>
+    </section>
+    )
 }
